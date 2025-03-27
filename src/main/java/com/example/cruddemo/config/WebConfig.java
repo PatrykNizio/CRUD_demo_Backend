9c3b218b2a3413fd0fa9a25a3372d13dc25a5aae
PatrykNizio <patryknizio@interia.com>
2025-03-27 21:48:48 +0100

    adding vercel url to spring security

--- src/main/java/com/example/cruddemo/config/WebConfig.java
+++ src/main/java/com/example/cruddemo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("https://crud-demo-front-end.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
